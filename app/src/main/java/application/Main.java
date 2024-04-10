02892eb8b36e23284416e6b4fd63b8521f60283d
marcoweb <marcocarvalho.web@gmail.com>
2024-04-10 23:49:14 +0000

    .

--- app/src/main/java/application/Main.java
+++ app/src/main/java/application/Main.java
@@ -1,8 +1,25 @@
 package application;
 
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+import javax.sound.midi.SysexMessage;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        
+        Scanner scn = new Scanner(System.in);
+        try {
+            int valor = scn.nextInt();
+        } catch(NoSuchElementException ex) {
+            System.out.println("Exception Ocorreu");
+        } catch(Exception ex) {
+            System.out.println("IOException");
+        } finally {
+            scn.close();
+        }
     }
 }
 
